83f993b800eab3aac2961dc370c1b3b95b5e8d64
Pablo Poblete <ppoblete@vrgroup.cl>
2022-07-08 23:16:00 -0400

    primera version demo1 jpa

---
+++ src/test/java/cl/ejemplo/jpatutorial/JpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ejemplo.jpatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
